aecbe33d66c11e2003f0c9b9251c813b533da1ac
Bedo9041 <7407004+Bedo9041@users.noreply.github.com>
2020-05-08 23:13:08 +0100

    Updated legacy api URL

--- src/main/java/me/bed0/jWynn/config/WynncraftAPIConfig.java
+++ src/main/java/me/bed0/jWynn/config/WynncraftAPIConfig.java
@@ -5,16 +5,23 @@ import me.bed0.jWynn.WynncraftAPI;
 public class WynncraftAPIConfig {
 
     private String baseURL;
+    private String baseURLLegacy;
     private boolean handleRatelimits;
     private String defaultUserAgent;
     private int defaultConnectionTimeout;
 
     public WynncraftAPIConfig() {
-        this("https://api.wynncraft.com/", true, "jWynn/v" + WynncraftAPI.VERSION, 10000);
+        this("https://api.wynncraft.com/", "https://api-legacy.wynncraft.com/", true, "jWynn/v" + WynncraftAPI.VERSION, 10000);
     }
 
+    @Deprecated
     public WynncraftAPIConfig(String baseURL, boolean handleRatelimits, String defaultUserAgent, int defaultConnectionTimeout) {
+        this(baseURL, "https://api-legacy.wynncraft.com/", handleRatelimits, defaultUserAgent, defaultConnectionTimeout);
+    }
+
+    public WynncraftAPIConfig(String baseURL, String baseURLLegacy, boolean handleRatelimits, String defaultUserAgent, int defaultConnectionTimeout) {
         this.baseURL = baseURL;
+        this.baseURLLegacy = baseURLLegacy;
         this.handleRatelimits = handleRatelimits;
         this.defaultUserAgent = defaultUserAgent;
         this.defaultConnectionTimeout = defaultConnectionTimeout;
@@ -22,6 +29,10 @@ public class WynncraftAPIConfig {
 
     public String getBaseURL() {
         return baseURL;
+    }
+
+    public String getBaseURLLegacy() {
+        return baseURLLegacy;
     }
 
     public boolean isHandleRatelimits() {
